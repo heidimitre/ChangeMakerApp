df319faa4d8a19fde3d86f889f5156d2c92d6ee6
heidimitre <heidi.mitre@gmail.com>
2015-02-15 09:15:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/heidi/myapplication/mainActivity.java
@@ -0,0 +1,233 @@
+package com.example.heidi.myapplication;
+
+import com.example.heidi.myapplication.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class mainActivity extends Activity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * If set, will toggle the system UI visibility upon interaction. Otherwise,
+     * will show the system UI visibility upon interaction.
+     */
+    private static final boolean TOGGLE_ON_CLICK = true;
+
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        final View controlsView = findViewById(R.id.fullscreen_content_controls);
+        final View contentView = findViewById(R.id.fullscreen_content);
+
+        // Set up an instance of SystemUiHider to control the system UI for
+        // this activity.
+        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
+        mSystemUiHider.setup();
+        mSystemUiHider
+                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+                    // Cached values.
+                    int mControlsHeight;
+                    int mShortAnimTime;
+
+                    @Override
+                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+                    public void onVisibilityChange(boolean visible) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+                            // If the ViewPropertyAnimator API is available
+                            // (Honeycomb MR2 and later), use it to animate the
+                            // in-layout UI controls at the bottom of the
+                            // screen.
+                            if (mControlsHeight == 0) {
+                                mControlsHeight = controlsView.getHeight();
+                            }
+                            if (mShortAnimTime == 0) {
+                                mShortAnimTime = getResources().getInteger(
+                                        android.R.integer.config_shortAnimTime);
+                            }
+                            controlsView.animate()
+                                    .translationY(visible ? 0 : mControlsHeight)
+                                    .setDuration(mShortAnimTime);
+                        } else {
+                            // If the ViewPropertyAnimator APIs aren't
+                            // available, simply show or hide the in-layout UI
+                            // controls.
+                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+                        }
+
+                        if (visible && AUTO_HIDE) {
+                            // Schedule a hide().
+                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                        }
+                    }
+                });
+
+        // Set up the user interaction to manually show or hide the system UI.
+        contentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TOGGLE_ON_CLICK) {
+                    mSystemUiHider.toggle();
+                } else {
+                    mSystemUiHider.show();
+                }
+            }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+    }
+
+    public void onButtonClick(View v)
+    {
+        EditText price = (EditText)findViewById(R.id.itemPrice);
+        TextView q1 = (TextView)findViewById(R.id.quarters);
+        TextView d1 = (TextView)findViewById(R.id.dimes);
+        TextView n1 = (TextView)findViewById(R.id.nickels);
+        TextView e1 = (TextView)findViewById(R.id.errorView);
+        e1.setText("");
+
+
+        TextView instructions = (TextView)findViewById(R.id.instructions);
+        TextView quarterV = (TextView)findViewById(R.id.quarterView);
+        TextView dimeV = (TextView)findViewById(R.id.dimeView);
+        TextView nickelV = (TextView)findViewById(R.id.nickelView);
+
+        instructions.setKeyListener(null);
+        quarterV.setKeyListener(null);
+        dimeV.setKeyListener(null);
+        nickelV.setKeyListener(null);
+
+
+        int itemPrice;
+        int changeAmount;
+        int quarterCounter;
+        int dimeCounter;
+        int nickelCounter;
+
+       try
+       {
+           itemPrice = Integer.parseInt(price.getText().toString());
+       }
+       catch(Exception ex)
+       {
+           e1.setText("Please enter an integer for price.");
+           return;
+       }
+
+
+       if(itemPrice > 100)
+       {
+           e1.setText("Price must be less than 1 dollar.");
+           return;
+       }
+
+       if(itemPrice <= 0)
+       {
+           e1.setText("Price must be greater than 0.");
+           return;
+       }
+
+       if(itemPrice % 5 != 0)
+       {
+           e1.setText("Price must be a multiple of 5 cents.");
+           return;
+       }
+
+
+        changeAmount = 100 - itemPrice;
+        quarterCounter = changeAmount / 25;
+        changeAmount = changeAmount % 25;
+        dimeCounter = changeAmount / 10;
+        changeAmount = changeAmount % 10;
+        nickelCounter = changeAmount / 5;
+
+        q1.setText(Integer.toString(quarterCounter));
+        d1.setText(Integer.toString(dimeCounter));
+        n1.setText(Integer.toString(nickelCounter));
+
+
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+}
